9d85c928613d8a74fa870868bcd689f5b18bd38a
Sean O'Connor <soconnor@yahoo.com>
2020-12-28 20:42:23 -0600

    Remove blank lines

--- src/test/kotlin/CommandLineUtilTest.kt
+++ src/test/kotlin/CommandLineUtilTest.kt
@@ -10,7 +10,6 @@ class CommandLineUtilTest {
     @Test
     fun stringParamFound() {
         val args = arrayOf("/a:b", "/c:d")
-
         assertEquals("b", com.hubbins.paramutil.param("a", null, args))
         assertEquals("d", com.hubbins.paramutil.param("c", null, args))
     }
@@ -18,7 +17,6 @@ class CommandLineUtilTest {
     @Test
     fun stringParamDefault() {
         val args = arrayOf("/a:b", "/c:d")
-
         assertEquals("test", com.hubbins.paramutil.param("something", "test", args))
         assertNull(com.hubbins.paramutil.param("something", null, args))
     }
@@ -26,28 +24,24 @@ class CommandLineUtilTest {
     @Test
     fun intParamFound() {
         val args = arrayOf("/a:123", "/c:d")
-
         assertEquals(123, com.hubbins.paramutil.param("a", 0, args))
     }
 
     @Test
     fun intParamDefault() {
         val args = arrayOf("/a:123", "/c:d")
-
         assertEquals(456, com.hubbins.paramutil.param("c", 456, args))
     }
 
     @Test
     fun argFound() {
         val args = arrayOf("/a", "/c:d")
-
         assertTrue(com.hubbins.paramutil.param("a", args))
     }
 
     @Test
     fun argNotFound() {
         val args = arrayOf("/a", "/c:d")
-
         assertFalse(com.hubbins.paramutil.param("test", args))
     }
 }
